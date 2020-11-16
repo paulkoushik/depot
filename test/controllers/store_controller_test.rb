9aa6dbf4b73d06e851beea569fc2447e91bcba2e
paulkoushik <koushik1995.paul@gmail.com>
2020-11-16 14:35:43 +0000

    added cart and line item models.

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get store_index_url
+    get store_url
     assert_response :success
     assert_select 'nav.side_nav a', minimum: 4
     assert_select 'main ul.catalog li', 3
-    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select 'h2', 'MyString1'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
