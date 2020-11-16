3a8f446c24d08c2a9d254dde6e33996e108b4b11
paulkoushik <koushik1995.paul@gmail.com>
2020-11-16 12:51:54 +0000

    added concerns

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,3 +3,5 @@ class StoreController < ApplicationController
       @products = Product.order(:title)
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_113036) do
+ActiveRecord::Schema.define(version: 2020_11_16_124301) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
