ad9a877f8a64169da689427e4684ce81cbcc42cd
paulkoushik <koushik1995.paul@gmail.com>
2020-11-23 14:54:58 +0000

    updated db/migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_112253) do
+ActiveRecord::Schema.define(version: 2020_11_18_114149) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
